c931c8a8dec306c973c13b27a400ea4b6a3877c3
Trishia El Chemaly <tchemaly@stanford.edu>
2024-06-05 09:13:48 -0700

    updated whisper labeling

--- Assets/WhisperEdited.cs
+++ Assets/WhisperEdited.cs
@@ -8,7 +8,7 @@ namespace Samples.Whisper
 {
     public class WhisperEdited : MonoBehaviour
     {
-        [SerializeField] private TMP_Text message; 
+        [SerializeField] private TMP_Text message;
 
         private readonly string fileName = "output.wav";
         private readonly int duration = 5;
@@ -23,8 +23,10 @@ namespace Samples.Whisper
 
         public GameObject ScriptManager;
 
-        private void Start()
+        private void OnEnable()
         {
+            isRecording = false;
+            time = 0;
             Speakobj.SetActive(true);
 
             if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
@@ -32,33 +34,21 @@ namespace Samples.Whisper
                 Permission.RequestUserPermission(Permission.Microphone);
             }
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-            dropdown.options.Add(new Dropdown.OptionData("Microphone not supported on WebGL"));
-#else
-            // foreach (var device in Microphone.devices)
-            // {
-            //     dropdown.options.Add(new TMP_Dropdown.OptionData(device));
-            // }
-            // recordButton.onClick.AddListener(StartRecording);
             StartRecording();
-            // dropdown.onValueChanged.AddListener(ChangeMicrophone);
-
-            var index = PlayerPrefs.GetInt("user-mic-device-index");
-            // dropdown.SetValueWithoutNotify(index);
-#endif
         }
 
         private void StartRecording()
         {
+            if (isRecording) return;
+
             isRecording = true;
-#if !UNITY_WEBGL
-            // Start recording from the default microphone
             clip = Microphone.Start(null, false, duration, 44100);
-#endif
         }
 
         private async void EndRecording()
         {
+            Microphone.End(null); // Stop the microphone after recording
+
             message.text = "Transcripting...";
 
             byte[] data = SaveWav.Save(fileName, clip);
@@ -74,17 +64,15 @@ namespace Samples.Whisper
             if (!string.IsNullOrEmpty(res.Text))
             {
                 message.text = res.Text;
-
                 ScriptManager.GetComponent<bb>().UpdateLabelText(res.Text);
-
                 Speakobj.SetActive(false);
-
             }
             else
             {
-                    message.text = "Transcription successful but no text returned.";
+                message.text = "Transcription successful but no text returned.";
             }
 
+            this.gameObject.SetActive(false); // Disable this GameObject
         }
 
         private void Update()
